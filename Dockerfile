fb255ec4d62cdf782a4742a16a84af0a36bcab37
Jonathan Hawk <jonathan@libreworks.com>
2015-06-15 11:52:09 -0400

    Adding Docker files

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM nginx:latest
+MAINTAINER Jonathan Hawk <jonathan@appertly.com>
+
+ADD mime.types /etc/nginx/mime.types
+ADD nginx.conf /etc/nginx/nginx.conf
+ADD ssl.conf /etc/nginx/conf.d/ssl.conf
+ADD default.conf /etc/nginx/conf.d/default.conf
+
+ADD start.sh /scripts/start.sh
+RUN chmod +x /scripts/start.sh
+
+CMD ["/scripts/start.sh"]
---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/conf.d/default.conf
+
+if [ ! -e "/etc/ssl/dhparam.pem" ]
+then
+  openssl dhparam -out "/etc/ssl/dhparam.pem" 2048 2>/dev/null
+fi
+
+if [ ! -e "/etc/ssl/cert.pem" ] || [ ! -e "/etc/ssl/private/key.pem" ]
+then
+  openssl req -x509 -newkey rsa:4096 \
+    -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=$HOSTNAME" \
+    -keyout "/etc/ssl/private/key.pem" \
+    -out "/etc/ssl/cert.pem" \
+    -days 3650 -nodes -sha256 2>/dev/null
+fi
+
+exec nginx -g "daemon off;"
